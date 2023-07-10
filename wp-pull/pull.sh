319063d839e5855c555006c3c15ebabe6785a015
jumber.balkhamishvili <j.balkhamishvili@gmail.com>
2023-07-10 18:02:27 +0400

    update pull.sh

--- wp-pull/pull.sh
+++ wp-pull/pull.sh
@@ -12,5 +12,5 @@ wp plugin install wp-migrate-db --allow-root --path=/var/www/html --activate
 
 # pull in data from WP Engine
 # you will need to fill in the details of the following command
-wp migratedb pull https://phpmyadmin.wpengine.com/ --allow-root --path=/var/www/html \
-  --username=jumber.balkhamishvili@tipalti.com --password=Nemo22nemo@@
+wp migratedb pull genfdev.sftp.wpengine.com --allow-root --path=/var/www/html \
+  --username=genfdev-codespaces-jaba-test --password=tipalti123@@
